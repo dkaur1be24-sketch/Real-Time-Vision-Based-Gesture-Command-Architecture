322f4c2ec08295d5ea1a7d33f6a5d73a9603c474
diljeet kaur <dkaur1_be24@thapar.edu>
2026-04-27 21:05:20 +0530

    Create training.py

---
+++ training.py
@@ -0,0 +1,12 @@
+"""
+No training is performed in this project.
+
+Reason:
+- Uses pre-trained MediaPipe models
+- Uses pre-trained face recognition encodings
+
+This file is included only to satisfy project structure requirements.
+"""
+
+def train_model():
+    print("No training required. Pre-trained models are used.")
